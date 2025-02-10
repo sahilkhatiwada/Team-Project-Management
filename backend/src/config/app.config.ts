67de1b94e7e7afc9b987d9ede8398cdd70e723ca
Sahil Khatiwada <51027994+sahilkhatiwada@users.noreply.github.com>
2025-02-10 21:29:08 +0545

    get-env&&appConfig

---
+++ backend/src/config/app.config.ts
@@ -0,0 +1,23 @@
+import { getEnv } from "../utils/get-env";
+
+const appConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  PORT: getEnv("PORT", " 3000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+  MONGO_DB_URL: getEnv("MONGO_DB_URL", ""),
+
+  SESSION_SECRET: getEnv("SESSION_SECRET"),
+  SESSION_EXPIRES_IN: getEnv("SESSION_EXPIRES_IN", "1d"),
+
+  GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID"),
+  GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET"),
+  GOOGLE_CALLBACK_URL: getEnv("GOOGLE_CALLBACK_URL"),
+
+  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
+  FRONTEND_GOOGLE_CALLBACK_URL: getEnv(
+    "FRONTEND_GOOGLE_CALLBACK_URL",
+    "http://localhost:3000/auth/google/callback"
+  ),
+});
+
+export const config = appConfig();
